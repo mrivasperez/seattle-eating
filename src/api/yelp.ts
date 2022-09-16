d68cf3ccf0ca14d2cacda7175892af2fde4d3d6a
migue <35717991+mrivasperez@users.noreply.github.com>
2022-09-15 17:36:50 -0700

    Update yelp.ts

--- src/api/yelp.ts
+++ src/api/yelp.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
+  // this is a fun API to learn with, enjoy :)  
     Authorization:
       'Bearer _OelRiWGwgDMtfBaC7c1ONqHrCNXbDYHp_V4pF1vAS6yBC2Yn5dT16DKpMwTEe10lXk0FJ6oEBMiPYQNvJ15fOqkrOJ5mE1pOQK_jQpRGHFuyCBIxsDehAa4VE4VY3Yx',
   },
